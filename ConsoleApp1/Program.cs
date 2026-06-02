43ad1c2d1987c1701bb8ef36e70900b41c4dac78
YE0250OCHI <tochi@youtheng.co.jp>
2026-06-02 13:11:18 +0900

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,11 @@
 ﻿Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Hello, World!");
+ShowConsole(1);
+
+
+void ShowConsole(int? input)
+{
+    int value = input.Value;
+
+    Console.WriteLine($"{value}");
+}
